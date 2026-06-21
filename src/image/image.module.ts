a564c5cf031486f3222412d0de620996299a65f0
성태경 <taegyeong0320@naver.com>
2026-06-21 22:21:35 +0900

    이미지 모듈 구현하기

---
+++ src/image/image.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ImageController } from './image.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Image } from './image.entity';
+import { AuthModule } from 'src/auth/auth.module';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Image]), AuthModule],
+  controllers: [ImageController],
+})
+export class ImageModule {}
--- src/post/post.entity.ts
+++ src/post/post.entity.ts
@@ -5,12 +5,14 @@ import {
   DeleteDateColumn,
   Entity,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 import { MarkerColor } from './marker-color.enum';
 import { ColumnNumericTransformer } from 'src/@common/transformers/numeric.transformer';
 import { User } from 'src/auth/user.entity';
+import { Image } from 'src/image/image.entity';
 
 @Entity()
 export class Post extends BaseEntity {
@@ -61,4 +63,7 @@ export class Post extends BaseEntity {
 
   @ManyToOne(() => User, (user) => user.post, { eager: false })
   user: User;
+
+  @OneToMany(() => Image, (image) => image.post)
+  images: Image[];
 }
